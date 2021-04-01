e65f0a29ab3173857e5c9eba7cc3ef71fca19989
gadzik <glina41@o2.pl>
2021-04-01 20:47:14 +0200

    Create products service

---
+++ products-service/app/api/products.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+products = APIRouter()
---
+++ products-service/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from app.api import database, engine, metadata
+from app.api.auth import session
+from app.api.products import products
+
+metadata.create_all(engine)
+
+app = FastAPI(openapi_url="/api/products/openapi.json", docs_url="/api/products/docs")
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+    await session.close()
+
+
+app.include_router(products, prefix='/api/products', tags=['products'])
